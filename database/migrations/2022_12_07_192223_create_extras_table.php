2b89f6ea40c7092625238fae8fed1a8dd0f35414
Joseph Makram <jojomak350@gmail.com>
2023-05-20 13:25:55 +0300

    Fix image column in extras

--- database/migrations/2022_12_07_192223_create_extras_table.php
+++ database/migrations/2022_12_07_192223_create_extras_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->double('price', 6, 2);
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->foreignId('store_id')
                 ->constrained('stores')
                 ->cascadeOnUpdate()
